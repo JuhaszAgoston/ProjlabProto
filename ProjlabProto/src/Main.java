071eb94fa4a2d39c8b03bca8423489d8ee70ef58
radvanyiakos <radvanyiakos@radvanyi.sch.bme.hu>
2019-04-22 22:55:32 +0200

    Teszt függvények clean

--- ProjlabProto/src/Main.java
+++ ProjlabProto/src/Main.java
@@ -69,311 +69,63 @@ public class Main {
 	}
 
 	public static void Teszt1() {
-		// Teszt1 inic
-		System.out.println("-------inic------");
-		Orangutan o = new Orangutan();
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		cs1.SetElem(o);
-		o.SetPosition(cs1);
-		cs2.AddNeighbour(cs1);
-		cs1.AddNeighbour(cs2);
-		// Teszt1 run
-		System.out.println("-------run------");
-		o.Move(cs2);
-
+		
 	}
 
 	public static void Teszt2() {
-		// Teszt2 inic
-		System.out.println("-------inic------");
-		Orangutan o = new Orangutan();
-		JatekGep j = new JatekGep();
-		Csempe ocs = new Csempe();
-		Csempe jcs = new Csempe();
-		ocs.SetElem(o);
-		o.SetPosition(ocs);
-		jcs.SetElem(j);
-		j.SetPosition(jcs);
-		ocs.AddNeighbour(jcs);
-		jcs.AddNeighbour(ocs);
-		// Teszt2 run
-		System.out.println("-------run------");
-		jcs.SteppedOnByOrangutan(o);
-
+		
 	}
 
 	public static void Teszt3() {
-		// Teszt3 inic
-		System.out.println("-------inic------");
-		Orangutan o = new Orangutan();
-		Csempe cs1 = new Csempe();
-		GyengeCsempe cs2 = new GyengeCsempe();
-		cs1.SetElem(o);
-		o.SetPosition(cs1);
-		cs2.AddNeighbour(cs1);
-		cs1.AddNeighbour(cs2);
-		cs2.SetLife(10);
-		// Teszt3 run
-		System.out.println("-------run------");
-		cs2.SteppedOnByOrangutan(o);
+
 	}
 
 	public static void Teszt4() {
-		// Teszt4 inic
-		System.out.println("-------inic------");
-		Orangutan o = new Orangutan();
-		Csempe cs1 = new Csempe();
-		GyengeCsempe cs2 = new GyengeCsempe();
-		cs1.SetElem(o);
-		o.SetPosition(cs1);
-		cs2.AddNeighbour(cs1);
-		cs1.AddNeighbour(cs2);
-		cs2.SetLife(0);
-		// Teszt4 run
-		System.out.println("-------run------");
-		cs2.SteppedOnByOrangutan(o);
 
 	}
 
 	public static void Teszt5() {
-		// Teszt5 inic
-		System.out.println("-------inic------");
-		GyengeCsempe gycs = new GyengeCsempe();
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Csempe cs3 = new Csempe();
-		Orangutan o = new Orangutan();
-		Panda p1 = new KekPanda();
-		Panda p2 = new KekPanda();
-		gycs.AddNeighbour(cs1);
-		cs1.AddNeighbour(gycs);
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		cs2.AddNeighbour(cs3);
-		cs3.AddNeighbour(cs2);
-		o.SetPosition(cs1);
-		cs1.SetElem(o);
-		p1.SetPosition(cs2);
-		cs2.SetElem(p1);
-		p2.SetPosition(cs3);
-		cs3.SetElem(p2);
-		o.AddPandaToSor(p1);
-		o.AddPandaToSor(p2);
-		p1.SetLeader(o);
-		p2.SetLeader(o);
-		gycs.SetLife(0);
-		// Teszt5 run
-		System.out.println("-------run------");
-		gycs.SteppedOnByOrangutan(o);
 
 	}
 
 	public static void Teszt6() {
-		// Teszt6 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		GyengeCsempe cs2 = new GyengeCsempe();
-		Panda p = new KekPanda();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		cs1.SetElem(p);
-		p.SetPosition(cs1);
-		cs2.SetLife(0);
-		// Teszt6 run
-		System.out.println("-------run------");
-		cs2.SteppedOnByPanda(p);
 
 	}
 
 	public static void Teszt7() {
-		// Teszt7 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Panda p = new KekPanda();
-		Orangutan o = new Orangutan();
-		cs1.SetElem(p);
-		p.SetPosition(cs1);
-		cs2.SetElem(o);
-		o.SetPosition(cs2);
-		p.SetFree(true);
-		// Teszt7 run
-		System.out.println("-------run------");
-		p.CollideWithOrangutan(o);
 
 	}
 
 	public static void Teszt8() {
-		// Teszt8 inic
-		System.out.println("-------inic------");
-		Orangutan o = new Orangutan();
-		Szekreny sz = new Szekreny();
-		Csempe ocs = new Csempe();
-		Csempe szcs = new Csempe();
-		Csempe target = new Csempe();
-		ocs.SetElem(o);
-		o.SetPosition(ocs);
-		szcs.SetElem(sz);
-		sz.SetPosition(szcs);
-		ocs.AddNeighbour(szcs);
-		szcs.AddNeighbour(ocs);
-		sz.setTarget(target);
-		target.SetElem(null);
-		// Teszt8 run
-		System.out.println("-------run------");
-		sz.CollideWithOrangutan(o);
-
+		
 	}
 
 	public static void Teszt9() {
-		// Teszt9 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Fotel f = new Fotel();
-		Panda zp = new ZoldPanda();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		f.SetPosition(cs1);
-		cs1.SetElem(f);
-		zp.SetPosition(cs2);
-		cs2.SetElem(zp);
-		// Teszt9 run
-		System.out.println("-------run------");
-		zp.AffectedByFotel(f);
-
+		
 	}
 
 	public static void Teszt10() {
-		// Teszt10 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		JatekGep j = new JatekGep();
-		Panda pp = new PirosPanda();
-		Orangutan l = new Orangutan();
-		Csempe lcs = new Csempe();
-		l.SetPosition(lcs);
-		lcs.SetElem(l);
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		j.SetPosition(cs1);
-		cs1.SetElem(j);
-		pp.SetPosition(cs2);
-		cs2.SetElem(pp);
-		pp.SetLeader(l);
-		l.AddPandaToSor(pp);
-		// Teszt10 run
-		System.out.println("-------run------");
-		pp.AffectedByJatekgep(j);
 
 	}
 
 	public static void Teszt11() {
-		// Teszt11 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		CsokiAutomata csa = new CsokiAutomata();
-		Panda kp = new KekPanda();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		csa.SetPosition(cs1);
-		cs1.SetElem(csa);
-		kp.SetPosition(cs2);
-		cs2.SetElem(kp);
-		// Teszt11 run
-		System.out.println("-------run------");
-		kp.AffectedByCsokiautomata(csa);;
-
+		
 	}
 
 	public static void Teszt12() {
-		// Teszt12 inic
-		System.out.println("-------inic------");
-		Kijarat k = new Kijarat();
-		Bejarat b = new Bejarat();
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Csempe cs3 = new Csempe();
-		Orangutan o = new Orangutan();
-		Panda p1 = new KekPanda();
-		Panda p2 = new KekPanda();
-		k.AddNeighbour(cs1);
-		cs1.AddNeighbour(k);
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		cs2.AddNeighbour(cs3);
-		cs3.AddNeighbour(cs2);
-		o.SetPosition(cs1);
-		cs1.SetElem(o);
-		p1.SetPosition(cs2);
-		cs2.SetElem(p1);
-		p2.SetPosition(cs3);
-		cs3.SetElem(p2);
-		o.AddPandaToSor(p1);
-		o.AddPandaToSor(p2);
-		p1.SetLeader(o);
-		p2.SetLeader(o);
-		// Teszt12 run
-		System.out.println("-------run------");
-		k.SteppedOnByOrangutan(o);
 
 	}
 
 	public static void Teszt13() {
-		// Teszt13 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Orangutan o = new Orangutan();
-		Panda p1 = new KekPanda();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		cs1.SetElem(o);
-		o.SetPosition(cs1);
-		p1.SetPosition(cs2);
-		cs2.SetElem(p1);
-		o.AddPandaToSor(p1);
-		// Teszt13 run
-		System.out.println("-------run------");
-		p1.CollideWithOrangutan(o);
-		
 
 	}
 
 	public static void Teszt14() {
-		// Teszt14 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Panda p1 = new KekPanda();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		p1.SetPosition(cs1);
-		cs1.SetElem(p1);
-		// Teszt14 run
-		System.out.println("-------run------");
-		p1.Step();
+
 	}
 
 	public static void Teszt15() {
-		// Teszt15 inic
-		System.out.println("-------inic------");
-		Csempe cs1 = new Csempe();
-		Csempe cs2 = new Csempe();
-		Panda p1 = new KekPanda();
-		JatekGep j = new JatekGep();
-		cs1.AddNeighbour(cs2);
-		cs2.AddNeighbour(cs1);
-		p1.SetPosition(cs1);
-		cs1.SetElem(p1);
-		j.SetPosition(cs2);
-		cs2.SetElem(j);
-		// Teszt15 run
-		System.out.println("-------run------");
-		p1.Step();
+	
 	}
 
 	public static void main(String[] args) throws IOException {
